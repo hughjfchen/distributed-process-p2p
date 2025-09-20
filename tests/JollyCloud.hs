857a1d22a4d9068ab183913e16d22ce2838ee6ae
Hugh JF Chen <hugh.jf.chen@gmail.com>
2025-09-20 08:57:26 +0800

    clean up warnings and reformat.

--- tests/JollyCloud.hs
+++ tests/JollyCloud.hs
@@ -1,74 +1,71 @@
 module Main where
 
+import Control.Concurrent (threadDelay)
 import qualified Control.Distributed.Backend.P2P as P2P
-import           Control.Distributed.Process as DP
-import           Control.Distributed.Process.Node as DPN
-
-import System.Environment (getArgs)
-
+import Control.Distributed.Process as DP
+import Control.Distributed.Process.Node as DPN
 import Control.Monad
 import Control.Monad.Trans (liftIO)
-import Control.Concurrent (threadDelay)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
   args <- getArgs
 
   case args of
-    host:port:seeds -> do
+    host : port : seeds -> do
       let ext = const (host, port)
       P2P.bootstrap host port ext initRemoteTable (map P2P.makeNodeId seeds) mainProcess
     _ -> putStrLn "Usage: jollycloud addr port [<seed>..]"
 
 mainProcess :: Process ()
 mainProcess = do
-    spawnLocal logger
+  spawnLocal logger
 
-    forever $ do
-        cmd <- liftIO getLine
-        case words cmd of
-            ["all"] -> listPeers
-            ["in", r] -> listRoom r
-            ["join", r] -> joinRoom r
-            ["part", r] -> partRoom r
-            "tell":r:msg -> tellRoom r (unwords msg)
-            _ -> liftIO . putStrLn $ "all | in <r> | join <r> | part <r> | tell <r> <msg>"
+  forever $ do
+    cmd <- liftIO getLine
+    case words cmd of
+      ["all"] -> listPeers
+      ["in", r] -> listRoom r
+      ["join", r] -> joinRoom r
+      ["part", r] -> partRoom r
+      "tell" : r : msg -> tellRoom r (unwords msg)
+      _ -> liftIO . putStrLn $ "all | in <r> | join <r> | part <r> | tell <r> <msg>"
 
 logger :: Process ()
 logger = do
-    unregister "logger"
-    getSelfPid >>= register "logger"
-    forever $ do
-        (time, pid, msg) <- expect :: Process (String, ProcessId, String)
-        liftIO $ putStrLn $ time ++ " " ++ show pid ++ " " ++ msg
-        return ()
+  unregister "logger"
+  getSelfPid >>= register "logger"
+  forever $ do
+    (time, pid, msg) <- expect :: Process (String, ProcessId, String)
+    liftIO $ putStrLn $ time ++ " " ++ show pid ++ " " ++ msg
+    return ()
 
 listPeers = P2P.getPeers >>= (liftIO . print)
 
 listRoom r = P2P.getCapable r >>= (liftIO . print)
 
 joinRoom r = do
-    pid <- whereis r
-    case pid of
-        Nothing -> spawnLocal (roomService r) >>= register r
-        Just _ -> return ()
+  pid <- whereis r
+  case pid of
+    Nothing -> spawnLocal (roomService r) >>= register r
+    Just _ -> return ()
 
 partRoom r = do
-    pid <- whereis r
-    case pid of
-        Nothing -> return ()
-        Just p -> send p (Nothing :: Maybe String)
+  pid <- whereis r
+  case pid of
+    Nothing -> return ()
+    Just p -> send p (Nothing :: Maybe String)
 
 tellRoom r msg = P2P.nsendCapable r (Just msg)
 
 roomService :: String -> Process ()
 roomService s = do
-    msg <- expect :: Process (Maybe String)
-    case msg of
-        Nothing -> do
-            liftIO . putStrLn $ "Leaving: " ++ s
-            unregister s
-        Just m -> do
-            liftIO . putStrLn $ "<" ++ s ++ "> " ++ m
-            roomService s
-
+  msg <- expect :: Process (Maybe String)
+  case msg of
+    Nothing -> do
+      liftIO . putStrLn $ "Leaving: " ++ s
+      unregister s
+    Just m -> do
+      liftIO . putStrLn $ "<" ++ s ++ "> " ++ m
+      roomService s
